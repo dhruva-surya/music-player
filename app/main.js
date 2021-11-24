d795b9b006c08b501201fc06ca3422aa99a45b25
dhruva-surya <dhruva_nj2_108@masai.school>
2021-11-25 00:34:52 +0530

    canvas box created

--- app/main.js
+++ app/main.js
@@ -1,6 +1,22 @@
 
 let flag=true;
 let audio=document.getElementById("audio");
+let progress=document.getElementById("progress");
+let progressCont=document.getElementById("progressContainer");
+let canvas=document.getElementById("canvas");
+
+let twoD=canvas.getContext("2d");
+
+
+ twoD.fillStyle="green"
+ twoD.fillRect(2,3,1,30)
+ twoD.fillRect(4,10,1,30-20)
+ twoD.fillRect(6,30-20,1,30-10)
+ 
+
+
+
+
 function playPause(){
     let button=document.getElementById("playAndPause")
     
@@ -17,11 +33,29 @@ function playPause(){
 }
 
 
-audio.ontimeupdate = function() {myFunction()};
 
-function myFunction() {
 
-  console.log(audio.currentTime)
+audio.addEventListener("timeupdate",updateProgress);
+let i=0;
+function updateProgress(e){
+  
+  const {duration,currentTime}=e.srcElement;
+  var progressPer=(currentTime/duration)*100;
+ 
+ // progress.style.width=`${progressPer}%`
+ 
+ 
 }
+
+canvas.addEventListener("click",setProgress)
+
+function setProgress(e){
+ const width=this.clientWidth;
+ const clickX=e.offsetX;
+  const duration=audio.duration;
+  audio.currentTime=(clickX/width)*duration;
+
+}
+
 
 
