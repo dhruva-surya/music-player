d619ce7ee366f3effa37ebd117f420bd980c610d
dhruva-surya <dhruva_nj2_108@masai.school>
2021-11-25 20:27:37 +0530

    canvas completed

--- app/main.js
+++ app/main.js
@@ -5,25 +5,31 @@ let progress=document.getElementById("progress");
 let progressCont=document.getElementById("progressContainer");
 let canvas=document.getElementById("canvas");
 
-console.log(canvas.clientWidth)
+
+
+
+
+
 
 let twoD=canvas.getContext("2d");
+
+
+
+
 
 
 
 let harr=[];
 let yarr=[];
 
-//  twoD.fillStyle="red"
-//  twoD.fillRect(0,14,1,17);
- twoD.fillStyle="black";
+
 
 
 
  for(let i=2;i<=298;i=i+2){
-     let h=Math.floor(Math.random() * 20)+5;
-    let y= Math.floor(Math.random() * 6)+8;
-  
+     let h=Math.floor(Math.random() * (20-5)+5);
+    let y= Math.floor(Math.random() * (32-27)+27);
+    console.log(h)
   
   harr.push(h);
   yarr.push(y);
@@ -31,11 +37,31 @@ let yarr=[];
  }
 
  function dis(x){
+twoD.fillStyle="greenyellow"
+twoD.fillRect(21,7,1,21);
+twoD.beginPath();
+twoD.fillStyle = "greenyellow";
+twoD.arc(21.5, 30, 2, 0, 2 * Math.PI);
+twoD.strokeStyle = "greenyellow";
+twoD.fill();
+twoD.stroke();
+
+
+twoD.fillStyle="#0288D1"
+twoD.fillRect(49,7,1,21);
+twoD.beginPath();
+twoD.fillStyle = "g#0288D1";
+twoD.arc(49.5, 30, 2, 0, 2 * Math.PI);
+twoD.strokeStyle = "#0288D1";
+twoD.fill();
+twoD.stroke();
+     
+    
      let j=0;
      twoD.fillStyle="black";
   for(let i=2;i<=298;i=i+2){
      if(i==x){ 
-   twoD.fillStyle="red"
+   twoD.fillStyle="#9E9E9E"
      }
      twoD.fillRect(i,yarr[j],1,harr[j])
     j++
@@ -47,16 +73,7 @@ let yarr=[];
 
  dis()
 
-// function dis1(i){
-  
- 
-//   for(let k=0;k<arr.length;k++){
-//       arr[k]
-//   }
-//   console.log(arr)
 
-
-// }
 
 
 
@@ -73,6 +90,7 @@ function playPause(){
         audio.pause();
         flag=true;
         button.innerHTML="&#9658"
+       
     }
   
 }
@@ -81,9 +99,8 @@ function playPause(){
 
 
 audio.addEventListener("timeupdate",updateProgress);
-let i=0;
-let j=1;
 
+let prev=0;
 
 
 
@@ -96,19 +113,17 @@ function updateProgress(e){
         var progressPer=(currentTime/duration)*100;
         let x=Math.floor(progressPer*2.98);
       
-     // progress.style.width=`${progressPer}%`
-    //    twoD.fillStyle="red" 
-    //    if(x%2==0){ 
-    //      if(x!=prev)  {
-    //  twoD.fillRect(x,yarr[j],1,harr[j]);
-    //  j++;
-    //  prev=x
-    //      }
-    //    }
- 
-    twoD.clearRect(0, 0, canvas.width, canvas.height);
+     
+       twoD.fillStyle="red" 
+       if(x%2==0){ 
+         if(x!=prev)  {
+            twoD.clearRect(0, 0, canvas.width, canvas.height);
       
-    dis(x);
+            dis(x);
+           prev=x;
+         }
+       }
+ 
    
 
 
